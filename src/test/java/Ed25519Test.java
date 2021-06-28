6548c1edef10537204d6f6b78749bb2a9d2e4588
Kaloyan Tanev <tanevdev@gmail.com>
2021-06-28 15:29:25 +0200

    Add Ed25519 tests

---
+++ src/test/java/Ed25519Test.java
@@ -0,0 +1,84 @@
+import com.ltonetwork.seasalt.Curve;
+import com.ltonetwork.seasalt.Digest;
+import com.ltonetwork.seasalt.KeyPair;
+import com.ltonetwork.seasalt.sign.Ed25519;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+public class Ed25519Test {
+
+    @Test
+    public void testKeyPair() {
+        KeyPair myKeyPair = Ed25519.keyPair();
+
+        Assertions.assertNotNull(myKeyPair.getPrivatekey());
+        Assertions.assertNotNull(myKeyPair.getPublickey());
+        Assertions.assertEquals(myKeyPair.getCurve(), Curve.Ed25519);
+    }
+
+    @Test
+    public void testKeyPairFromSeed() {
+        Random rd = new Random();
+        byte[] b = new byte[64];
+        rd.nextBytes(b);
+
+        KeyPair myKeyPair = Ed25519.keyPairFromSeed(b);
+
+        Assertions.assertNotNull(myKeyPair.getPrivatekey());
+        Assertions.assertNotNull(myKeyPair.getPublickey());
+        Assertions.assertEquals(myKeyPair.getCurve(), Curve.Ed25519);
+    }
+
+    @Test
+    public void testKeyPairFromSecretKey() {
+        byte[] sk = Ed25519.keyPair().getPrivatekey();
+
+        KeyPair myKeyPair = Ed25519.keyPairFromSecretKey(sk);
+
+        Assertions.assertArrayEquals(sk, myKeyPair.getPrivatekey());
+        Assertions.assertNotNull(myKeyPair.getPublickey());
+        Assertions.assertEquals(myKeyPair.getCurve(), Curve.Ed25519);
+    }
+
+    @Test
+    public void testKeyPairFromSecretKeyFail() {
+        Random rd = new Random();
+        byte[] sk = new byte[12];
+        rd.nextBytes(sk);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Ed25519.keyPairFromSecretKey(sk);
+        });
+    }
+
+    @Test
+    public void testSigns() {
+        KeyPair kp = Ed25519.keyPair();
+        byte[] msg = "test".getBytes(StandardCharsets.UTF_8);
+
+        Assertions.assertDoesNotThrow(() -> {
+            Ed25519.signDetached(msg, kp, Digest.SHA256);
+        });
+    }
+
+    @Test
+    public void testVerify() {
+        KeyPair kp = Ed25519.keyPair();
+        byte[] msg = "test".getBytes(StandardCharsets.UTF_8);
+        byte[] sig = Ed25519.signDetached(msg, kp.getPrivatekey(), Digest.SHA256);
+
+        Assertions.assertTrue(Ed25519.verify(msg, sig, kp));
+    }
+
+    @Test
+    public void testVerifyFail() {
+        KeyPair kp = Ed25519.keyPair();
+        byte[] msg = "test".getBytes(StandardCharsets.UTF_8);
+        byte[] sig = Ed25519.signDetached(msg, kp.getPrivatekey(), Digest.SHA256);
+
+        Assertions.assertFalse(Ed25519.verify("fail".getBytes(StandardCharsets.UTF_8), sig, kp));
+    }
+}
