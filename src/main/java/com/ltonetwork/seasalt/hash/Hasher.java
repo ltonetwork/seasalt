22cb5a45c613084a394df9e90766f62ecb19f5db
Kaloyan Tanev <tanevdev@gmail.com>
2021-06-30 15:05:03 +0200

    Refactor to single class

--- src/main/java/com/ltonetwork/seasalt/hash/Hasher.java
+++ src/main/java/com/ltonetwork/seasalt/hash/Hasher.java
@@ -1,7 +1,31 @@
 package com.ltonetwork.seasalt.hash;
 
-public interface Hasher {
-    Digest hash(byte[] msg);
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-    Digest hash(String msg);
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.Security;
+
+public class Hasher {
+
+    MessageDigest md;
+
+    public Hasher(MessageDigest md) throws NoSuchAlgorithmException, NoSuchProviderException {
+        Security.addProvider(new BouncyCastleProvider());
+        this.md = md;
+    }
+
+    public Hasher(String algorithm) throws NoSuchAlgorithmException, NoSuchProviderException {
+        Security.addProvider(new BouncyCastleProvider());
+        this.md = MessageDigest.getInstance(algorithm, "BC");
+    }
+
+    public Digest hash(byte[] msg) {
+        return new Digest(md.digest(msg));
+    }
+
+    public Digest hash(String msg) {
+        return new Digest(md.digest(msg.getBytes()));
+    }
 }
