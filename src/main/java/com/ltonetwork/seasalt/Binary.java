0215f4d4b3a2ac024c539c441d0588a0b0d99ca3
Kaloyan Tanev <tanevdev@gmail.com>
2021-07-01 12:39:03 +0200

    Change Binary return of 'to's

--- src/main/java/com/ltonetwork/seasalt/Binary.java
+++ src/main/java/com/ltonetwork/seasalt/Binary.java
@@ -29,15 +29,15 @@ public class Binary {
         return Base64.getEncoder().encodeToString(digest);
     }
 
-    public static byte[] fromHex(String hex) throws DecoderException {
-        return Hex.decodeHex(hex);
+    public static Binary fromHex(String hex) throws DecoderException {
+        return new Binary(Hex.decodeHex(hex));
     }
 
-    public static byte[] fromBase58(String base58) {
-        return Base58.decode(base58);
+    public static Binary fromBase58(String base58) {
+        return new Binary(Base58.decode(base58));
     }
 
-    public static byte[] fromBase64(String base64) {
-        return Base64.getDecoder().decode(base64);
+    public static Binary fromBase64(String base64) {
+        return new Binary(Base64.getDecoder().decode(base64));
     }
 }
