a40495e67d46e4d5eeed0aa53bad207357656246
Kaloyan Tanev <tanevdev@gmail.com>
2021-09-22 11:45:56 +0200

    Add for loops to run threads longer

--- src/test/java/hash/Blake2b256Test.java
+++ src/test/java/hash/Blake2b256Test.java
@@ -27,18 +27,24 @@ public class Blake2b256Test {
     @Test
     public void testBlake2b_256_multithread() {
         Runnable task_1 = () -> {
-            String hexRes = "7ba3f3325eb69d507a7d7feb08297b36798c12245ebd6527d78099e94dac8bdd";
-            Assertions.assertEquals(hexRes, Blake2b256.hash(new Binary("task_1".getBytes())).getHex());
+            for(int i=0; i<100; i++){
+                String hexRes = "7ba3f3325eb69d507a7d7feb08297b36798c12245ebd6527d78099e94dac8bdd";
+                Assertions.assertEquals(hexRes, Blake2b256.hash(new Binary("task_1".getBytes())).getHex());
+            }
         };
 
         Runnable task_2 = () -> {
-            String hexRes = "43600450cff431c84c6a28e614aa708c7899ccad1aa4ed472bccd99de4e537bb";
-            Assertions.assertEquals(hexRes, Blake2b256.hash(new Binary("task_2".getBytes())).getHex());
+            for(int i=0; i<100; i++) {
+                String hexRes = "43600450cff431c84c6a28e614aa708c7899ccad1aa4ed472bccd99de4e537bb";
+                Assertions.assertEquals(hexRes, Blake2b256.hash(new Binary("task_2".getBytes())).getHex());
+            }
         };
 
         Runnable task_3 = () -> {
-            String hexRes = "f0259052aaca82ed016a69603ce020de190632b64643b5bcacbd47c813054b22";
-            Assertions.assertEquals(hexRes, Blake2b256.hash(new Binary("task_3".getBytes())).getHex());
+            for(int i=0; i<100; i++) {
+                String hexRes = "f0259052aaca82ed016a69603ce020de190632b64643b5bcacbd47c813054b22";
+                Assertions.assertEquals(hexRes, Blake2b256.hash(new Binary("task_3".getBytes())).getHex());
+            }
         };
 
         Thread thread_1 = new Thread(task_1);
